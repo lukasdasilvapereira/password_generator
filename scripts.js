b0398b9e3559886a42836dfaaba368aafdfabedc
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-07-01 13:03:33 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let range = document.getElementById("range").value
+let mainInput = document.getElementById("text").value
+let test = document.querySelector(".test")
+let little = document.getElementById("check")
+let big = document.getElementById("segundo")
+let numbers = document.getElementById("terceiro")
+let especiais = document.getElementById("quatro")
+
